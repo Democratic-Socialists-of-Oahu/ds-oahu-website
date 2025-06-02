06f6057761452295598ebed5f9f082a7f974e8ba
Rory Strahan-Mauk <rorystrahanmauk@gmail.com>
2025-06-01 21:00:59 -1000

    cmonnn

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import { env } from '$env/static/private';
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = postgres(env.DATABASE_URL);
+import { DATABASE_URL } from '$env/static/private';
+if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const client = postgres(DATABASE_URL);
 export const db = drizzle(client);
