2f2544505ae70035047f65d3ddb5af6ad762233e
Rory Strahan-Mauk <rorystrahanmauk@gmail.com>
2026-02-25 08:51:31 -1000

    trailing slash

--- svelte.config.js
+++ svelte.config.js
@@ -12,12 +12,12 @@ const config = {
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter(),
-		paths: {
-			base: process.argv.includes('dev') ? '' : '/ds-oahu-website'
-		},
-		prerender: {
-			handleHttpError: 'warn'
-		}
+        paths: {
+            base: process.argv.includes('dev') ? '/' : '/ds-oahu-website/'
+        },
+        prerender: {
+            handleHttpError: 'warn'
+        }
     }
 };
 
