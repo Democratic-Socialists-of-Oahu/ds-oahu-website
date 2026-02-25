f2111a0993a8c3a0004e59a10dc0c214e8e2eb10
Rory Strahan-Mauk <rorystrahanmauk@gmail.com>
2026-02-25 09:32:43 -1000

    no

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,20 @@
-import adapter from 'amplify-adapter';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
-              
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-        // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-        // for more information about preprocessors
-        preprocess: vitePreprocess(),
+    kit: {
 
-        kit: {
-                // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-                // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-                // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-                adapter: adapter()
+        adapter: adapter({
+            fallback: 'app.html'
+        }),
+        paths: {
+            base: process.argv.includes('dev') ? '' : '/ds-oahu-website'
+        },
+        prerender: {
+            default: true,
+            handleHttpError: 'warn'
         }
+    }
 };
 
 export default config;
