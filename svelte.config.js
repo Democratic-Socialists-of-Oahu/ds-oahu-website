e1b4fdeab60fd7439abea5dd60a7995c802ab791
Rory Strahan-Mauk <rorystrahanmauk@gmail.com>
2026-02-25 09:50:02 -1000

    pushing ahead

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,6 @@ const config = {
             base: process.argv.includes('dev') ? '' : '/ds-oahu-website'
         },
         prerender: {
-            default: true,
             handleHttpError: 'warn'
         }
     }
