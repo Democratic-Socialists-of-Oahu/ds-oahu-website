4626013f757fd6154676aa205f6671dc77bfb17b
Rory Strahan-Mauk <rorystrahanmauk@gmail.com>
2026-02-25 09:15:33 -1000

    who knows

--- svelte.config.js
+++ svelte.config.js
@@ -3,15 +3,14 @@ import adapter from 'amplify-adapter';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-
         adapter: adapter(),
-        paths: {
-            base: process.argv.includes('dev') ? '' : '/ds-oahu-website'
-        },
-        prerender: {
-            default: true,
-            handleHttpError: 'warn'
-        }
+        // paths: {
+        //     base: process.argv.includes('dev') ? '' : '/ds-oahu-website'
+        // },
+        // prerender: {
+        //     default: true,
+        //     handleHttpError: 'warn'
+        // }
     }
 };
 
