9f4855a64c9b180d083a5d022b6935d0c1ed5e67
rory strahan-mauk <rorystrahanmauk@gmail.com>
2026-07-02 15:20:42 -1000

    add dsa link

--- src/routes/join/+page.svelte
+++ src/routes/join/+page.svelte
@@ -2,7 +2,18 @@
 	<script src="https://actionnetwork.org/widgets/v5/form/join-dso?format=js&source=widget"></script>
 </svelte:head>
 
-<h3 style="text-align: center; padding-top: 20px;">
+<h3 style="text-align: left; padding-top: 20px;">
+	<a href="https://dsausa.org/join" target="_blank"
+		>To join DSO, first click here to join DSA!</a
+	>
+</h3>
+
+<h5 style="text-align: left; padding-top: 20px;">
+	To pay dues to DSO directly, click "Pay your dues!" in the navigation bar above!
+	Note that you must be a member of DSA to be a voting member in DSO.
+</h5>
+
+<h3 style="text-align: left; padding-top: 20px;">
 	<a href="https://actionnetwork.org/campaigns/welcome-to-dso/" target="_blank"
 		>Join our next welcome call! Click for information.</a
 	>
